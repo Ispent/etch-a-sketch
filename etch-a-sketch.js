0e554a2128f21d083a645737ed01efad9acd819c
patty lee <seunghak2003@gmail.com>
2024-01-23 14:58:47 -0500

    Create base game

---
+++ etch-a-sketch.js
@@ -0,0 +1,32 @@
+const grids = document.querySelectorAll('.grid');
+
+function blackPen() {
+  grids.forEach(grid => {
+    grid.addEventListener('mouseover', () => {
+      grid.classList.remove('erase')
+      grid.classList.add('black');
+    });
+  });
+}
+
+function erase() {
+  grids.forEach(grid => {
+    grid.addEventListener('mouseover', () => {
+      grid.classList.remove('black');
+      grid.classList.add('erase');
+    });
+  });
+}
+
+function penSelector() {
+  const blackBtn = document.querySelector('.blackPen');
+  const eraser = document.querySelector('.erase');
+  blackBtn.addEventListener('click', () => {
+    blackPen();
+  });
+  eraser.addEventListener('click', () => {
+    erase();
+  });
+}
+
+penSelector();
